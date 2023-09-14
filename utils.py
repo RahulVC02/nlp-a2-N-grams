bd12e7dc8327ee2992ce466ac89bab39639a0e26
SRocks04 <102377764+SRocks04@users.noreply.github.com>
2023-09-14 19:14:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -32,3 +32,21 @@ def load_data(data_dir):
 
 #Add Frequency Distribution code to
 #the codebase
+
+def counter(data, gram):        #data in the form of a single string, gram is the type 
+    vocab = data.split()
+
+    gram_gram = [""]
+    gram_gram.extend(vocab[:gram-1])
+
+    count_gram_dict = {}        #dictionary to store frequency of grams
+
+    for i in range (gram, len(vocab)):
+        gram_gram.pop(0)
+        gram_gram.append(vocab[i-1])
+
+        gram_element = " ".join(gram_gram)
+
+        count_gram_dict[gram_element] = count_gram_dict.get(gram_element,0) + 1
+
+    return count_gram_dict
