4ff248adae24d31fcd9b0998b8d7f29c28898ae6
rahulvc02 <rahul.chembakasseril@iitgn.ac.in>
2023-09-14 23:00:03 +0530

    completing preprocessing.

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,6 @@
-import pandas as pd
 import re
 import pandas as pd
-import nltk
+import numpy as np
 from joblib import Parallel, delayed
 from nltk.tokenize import sent_tokenize
 
@@ -21,16 +20,25 @@ processed_comments = Parallel(n_jobs=8)(delayed(pre_process)(text) for text in d
 #concatenate all comments into one string
 corpus = ""
 for comment in processed_comments:
-    corpus += comment + ' '
+    corpus += comment + '. '
 
-corpus_comments = []
+def remove_punct_post_tokenizing(sentence):
+    sentence = re.sub(r'[^a-zA-Z0-9 ]', '', sentence)
+    sentence = re.sub(r'x000d', ' ', sentence)      #remove common usernames
+    sentence = re.sub(r'x200b', ' ', sentence)      #remove common usernames
+    sentence = re.sub(r' +', ' ', sentence)              #remove extra spaces
+    return sentence
+
 sentences = sent_tokenize(corpus)
-for sentence in sentences:
-    sentence = re.sub(r'[^a-zA-Z0-9 ]', '', sentence)   #remove punctuation after tokenizing
-    corpus_comments.append(sentence)
+corpus_comments = Parallel(n_jobs=8)(delayed(remove_punct_post_tokenizing)(sentence) for sentence in sentences)
+sanitized_corpus_comments = np.array(corpus_comments)
+sanitized_corpus_comments = sanitized_corpus_comments[sanitized_corpus_comments != '']
 
-pd.Series(corpus_comments).to_csv('data/comments.csv', index=False, header=False)
-final_df = pd.read_csv('data/comments.csv', header=None)
+final_sent_df = pd.Series(sanitized_corpus_comments)
+final_sent_df = final_sent_df.dropna()
 
-print(len(final_df))
-print(final_df[:10])
+final_sent_df.to_csv('data/final_sentences_final.csv', index=False, header=False)
+final_sent_df = pd.read_csv('data/final_sentences_final.csv', header=None)
+
+print(len(final_sent_df))
+print(final_sent_df[:10])
