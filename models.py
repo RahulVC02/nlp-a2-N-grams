6153640d5d76f3d18a62a816f57ec891ebd81628
rahulvc02 <rahul.chembakasseril@iitgn.ac.in>
2023-09-16 22:57:50 +0530

    added default smoothing.

--- models.py
+++ models.py
@@ -5,16 +5,16 @@ all_sentences = pd.read_csv('data/final_sentences_final.csv', header=None)
 all_sentences = all_sentences.iloc[:,0]
 
 #sanity check
-train_data = all_sentences
-test_data = train_data
+# train_data = all_sentences
+# test_data = train_data
 
 split_ratio = 0.8
 all_sentences = all_sentences.sample(frac=1, random_state=42)  
 num_train = int(len(all_sentences) * split_ratio)
 
 #actual train and test data
-# train_data = all_sentences[:num_train]
-# test_data = all_sentences[num_train:]
+train_data = all_sentences[:num_train]
+test_data = all_sentences[num_train:]
 
 print("No. of Sentences in Train Data : ", len(train_data))
 print("No. of Sentences in Test Data : ", len(test_data))
@@ -24,7 +24,7 @@ smoothing_values = [None, 'laplace', 0.001, 0.01, 0.1, 0.5, 2, 5, 10, 'goodTurin
 
 #flag to find log perplexity or normal perplexity
 calculate_log_perplexity = True
-current_smoothing = None
+current_smoothing = 'laplace'
 print("Smoothing : ", current_smoothing)
 
 #ngram models
